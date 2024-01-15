4892bbc88176886fd492d9cc83bfd24495de4f48
Silver Zhang <silvertianren@gmail.com>
2024-01-15 11:33:52 -0500

    Remove spinlock of lock ops

--- arch/x86/kernel/rr_serialize.c
+++ arch/x86/kernel/rr_serialize.c
@@ -20,96 +20,76 @@ struct rr_lock_waiter {
     bool woken_up;
 };
 
-static struct rr_lock_waiter *cpu_waiters[MAX_CPU_NUM];
+// static struct rr_lock_waiter *cpu_waiters[MAX_CPU_NUM];
+
+static int cpu_wait_signal[MAX_CPU_NUM];
+static atomic_t current_spot;
 
 static struct rr_lock *exec_lock = NULL;
 
 
 static void rr_lock_init(struct rr_lock *lock) {
-    struct rr_lock_waiter *waiter;
+    int num_cpus = num_online_cpus();
 
     INIT_LIST_HEAD(&lock->queue);
     spin_lock_init(&lock->lock);
     lock->owner = -1; // Initialize to -1 indicating no owner
 
-    for (int i = 0; i < num_online_cpus(); i++) {
-        waiter = kmalloc(sizeof(struct rr_lock_waiter), GFP_KERNEL);
-        if (!waiter) {
-            // Handle allocation failure
-        }
-
-        waiter->cpu_id = i;
-
-        cpu_waiters[i] = waiter;
+    for (int i = 0; i < num_cpus; i++) {
+        cpu_wait_signal[i] = 0;
+        atomic_set(&current_spot, 0);
     }
+
+    cpu_wait_signal[num_cpus] = -2;
 }
 
 static void rr_lock(struct rr_lock *lock) {
-    unsigned long flags;
     int cpu_id;
-    struct rr_lock_waiter *waiter;
-    struct rr_lock_waiter *first_waiter;
+    int spot;
 
     preempt_disable();
-    spin_lock_irqsave(&lock->lock, flags);
 
     cpu_id = smp_processor_id();
 
-    if (lock->owner == cpu_id)
+    if (atomic_read(&current_spot) == cpu_id)
         goto out;
 
-    // Now owner now
-    if (lock->owner == -1) {
-        lock->owner = cpu_id;
-        goto out;
-    }
+    cpu_wait_signal[cpu_id] = 1;
 
-    waiter = cpu_waiters[cpu_id];
-
-    list_add_tail(&waiter->list, &exec_lock->queue);
-
-    while (lock->owner != cpu_id) {
-        spin_unlock_irqrestore(&lock->lock, flags);
+    while (1) {
+        spot = atomic_read(&current_spot);
+        if (spot == cpu_id)
+            break;
+        if (spot == -1) {
+            if (atomic_cmpxchg(&current_spot, -1, cpu_id) == -1)
+                break;
+        }
 
         cpu_relax();
-
-        spin_lock_irqsave(&lock->lock, flags);
     }
 
-    first_waiter = list_first_entry(&exec_lock->queue, struct rr_lock_waiter, list);
-
-    // I must be the first waiter once reaching here
-    BUG_ON(first_waiter->cpu_id != cpu_id);
-    list_del(&first_waiter->list); // Remove the head of the queue
+    cpu_wait_signal[cpu_id] = 0;
 
 out:
     preempt_enable();
-    spin_unlock_irqrestore(&lock->lock, flags);
 }
 
 static void rr_unlock(struct rr_lock *lock) {
-    unsigned long flags;
-    int cpu_id;
-    struct rr_lock_waiter *first_waiter;
+    // int cpu_id;
+    int i;
 
-    preempt_disable();
-    spin_lock_irqsave(&lock->lock, flags);
+    for (i = 0; i < MAX_CPU_NUM; i++) {
+        if (cpu_wait_signal[i] == -2) {
+            // No one is waiting
+            atomic_set(&current_spot, -1);
+            break;
+        }
 
-    cpu_id = smp_processor_id();
-
-    if (lock->owner != cpu_id)
-        goto out;
-
-    if (list_empty(&exec_lock->queue)) {
-        lock->owner = -1;
-    } else {
-        first_waiter = list_first_entry(&exec_lock->queue, struct rr_lock_waiter, list);
-        lock->owner = first_waiter->cpu_id;
+        if (cpu_wait_signal[i]) {
+            // CPU i is waiting
+            atomic_set(&current_spot, i);
+        }
     }
-
-out:
-    preempt_enable();
-    spin_unlock_irqrestore(&lock->lock, flags);
 }
 
 void init_smp_exec_lock(void)
