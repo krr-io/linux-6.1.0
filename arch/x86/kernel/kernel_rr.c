9e3333634cadcc78d7cf7e1a746c13adf9b408f7
Silver Zhang <silvertianren@gmail.com>
2024-10-19 08:22:02 -0400

    Add in-guest record of rdtsc

--- arch/x86/kernel/kernel_rr.c
+++ arch/x86/kernel/kernel_rr.c
@@ -2,6 +2,7 @@
 #include <asm/kernel_rr.h>
 #include <asm/traps.h>
 #include <linux/ptrace.h>
+#include <asm/msr.h>
 
 __visible noinstr void rr_record_syscall(struct pt_regs *regs)
 {
@@ -523,3 +524,34 @@ pte_t rr_read_pte_once(pte_t *pte)
 
     return rr_pte;
 }
+
+unsigned long *rr_rdtsc_begin(void)
+{
+    unsigned long flags;
+    void *event;
+    rr_io_input *input = NULL;
+
+    if (!rr_queue_inited()) {
+        return NULL;
+    }
+
+    if (!rr_enabled()) {
+        return NULL;
+    }
+
+    local_irq_save(flags);
+
+    event = rr_alloc_new_event_entry(sizeof(rr_io_input), EVENT_TYPE_RDTSC);
+    if (event == NULL) {
+        panic("Failed to allocate entry");
+    }
+
+    input = (rr_io_input *)event;
+
+    input->id = 0;
+
+    local_irq_restore(flags);
+
+    return &(input->value);
+}
+EXPORT_SYMBOL_GPL(rr_rdtsc_begin);
